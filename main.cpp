d156a6b4748d3df947f5a3367b1b2b82c3f6c262
Oliver Adams <oliveradams56@gmail.com>
2024-12-09 13:55:54 -0500

    completed the game

--- main.cpp
+++ main.cpp
@@ -6,26 +6,25 @@ using namespace std;
 const int screenWidth = 800;
 const int screenHeight = 650;
 
-enum GameScreen{START, PLAY, OVER}; // Game States
+enum GameScreen{START, PLAY, OVER};
+bool gamePaused = false;
 
-// Players scores
 int playerOneScore = 0;
 int playerTwoScore = 0;
 
-void drawGameBackground(); // Draw the game's background (same throughout)
-void drawStartScreen(Font); // Draw the start screen with the "CLICK TO START" text
-void drawGameOverScreen(Font); // Draw the game over screen with the "GAME OVER!!!" text
-void playGame();
+void drawGameBackground();
+void drawStartScreen(Font);
+void drawGameOverScreen(Font);
+void playGame(bool);
 
 int main()
 {
-	InitWindow(screenWidth, screenHeight, "Ping Pong"); // window size and name
-	GetMonitorCount() >= 2 ? SetWindowMonitor(2) : SetWindowMonitor(0); // check monitor to see how many and which is being used
+	InitWindow(screenWidth, screenHeight, "Ping Pong");
+	GetMonitorCount() >= 2 ? SetWindowMonitor(1) : SetWindowMonitor(0);
 	SetTargetFPS(60);
-	Font ButtonFont = LoadFont("../res/RubikMoonrocks-Regular.ttf"); //the font used through out the entirety of the game
-	GameScreen currentScreen = START; //game starts at this screen 
+	Font ButtonFont = LoadFont("../res/RubikMoonrocks-Regular.ttf");
+	GameScreen currentScreen = START;
 
-	//this is stuff happening in the game it self
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
@@ -42,11 +41,22 @@ int main()
 			drawStartScreen(ButtonFont);
 			break;
 		case PLAY:
-			if(IsKeyPressed(KEY_ENTER) || (playerOneScore == 5 || playerTwoScore == 5))
+			if((playerOneScore == 5 || playerTwoScore == 5))
 			{
 				currentScreen = OVER;
 			}
-			playGame();
+			if(IsKeyPressed(KEY_SPACE))
+			{
+				gamePaused = !gamePaused;
+			}
+			else if(IsKeyPressed(KEY_Q))
+			{
+				EndDrawing();
+				UnloadFont(ButtonFont);
+				CloseWindow();
+				return 0; 
+			}
+			playGame(gamePaused);
 			break;
 		case OVER:
 			drawGameOverScreen(ButtonFont);
@@ -75,23 +85,20 @@ int main()
 
 void drawGameBackground()
 {
-	// top line properties 
 	int topStartPosX = 0;
-	int topStartPosY = 10; //border between screen and line
+	int topStartPosY = 10;
 	int topEndPosX = screenWidth;
-	int topEndPosY = 10;  //border between screen and line
+	int topEndPosY = 10; 
 
-	// bottom line properties 
 	int bottomStartPosX = 0;
-	int bottomStartPosY = screenHeight-10; //border between screen and line
+	int bottomStartPosY = screenHeight-10;
 	int bottomEndPosX = screenWidth;
-	int bottomEndPosY = screenHeight-10; //border between screen and line
+	int bottomEndPosY = screenHeight-10;
 
-	// Middle line properties 
 	int MiddleStartPosX = screenWidth/2;
-	int MiddleStartPosY = 10; //border between screen and line
+	int MiddleStartPosY = 10;
 	int MiddleEndPosX = screenWidth/2;
-	int MiddleEndPosY = screenHeight-10; //border between screen and line
+	int MiddleEndPosY = screenHeight-10;
 
 	DrawLine(topStartPosX, topStartPosY, topEndPosX, topEndPosY, RAYWHITE);
 	DrawLine(bottomStartPosX, bottomStartPosY, bottomEndPosX, bottomEndPosY, RAYWHITE);
@@ -100,19 +107,16 @@ void drawGameBackground()
 
 void drawStartScreen(Font ButtonFont)
 {
-	// start text properties
 	string startButtonMessage = "PRESS ENTER TO START";
-	Vector2 startPosition = {50, 250}; // The text will be drawn a little off center of the screen
-	Vector2 startOrigin = { 0.5f, 0.5f }; // centered the center of the text
+	Vector2 startPosition = {50, 250};
+	Vector2 startOrigin = { 0.5f, 0.5f };
 	float fontRotation = 0.0f;
 	float fontSize = 50.0f;
 	float fontSpacing = 10.0f;
 
-	// ball properties
 	int ballCenterOnX = screenWidth/2;
 	int ballCenterOnY = screenHeight/2;
 	float ballRadius = 10;
-
 
 	DrawTextPro(ButtonFont, startButtonMessage.c_str(),startPosition, startOrigin, fontRotation, fontSize, fontSpacing, GREEN);
 	DrawCircle(ballCenterOnX, ballCenterOnY, ballRadius, GOLD);
@@ -133,17 +137,13 @@ void drawGameOverScreen(Font ButtonFont)
 		DrawTextPro(ButtonFont, "!!!! DRAW !!!!",{200, 15}, {0.5f, 0.5f}, 0, 50, 10.0f, PINK);
 	}
 
-	// Reset the scores when restarting the game
-
-	// Reset the scores when restarting the game
-
 	string GameOverButtonMessage1 = "GAME OVER!!!";
 	string GameOverButtonMessage2 = "PRESS ENTER TO GO AGAIN";
 	string GameOverButtonMessage3 = "PRESS Q TO QUIT";
-	Vector2 startPosition1 = {195, 250}; // The text will be drawn a little off center of the screen
+	Vector2 startPosition1 = {195, 250};
 	Vector2 startPosition2 = {0, 550};
 	Vector2 startPosition3 = {185, 600};
-	Vector2 startOrigin = { 0.5f, 0.5f }; // centered the center of the text
+	Vector2 startOrigin = { 0.5f, 0.5f };
 	float fontRotation = 0.0f;
 	float fontSize = 50.0f;
 	float fontSpacing = 10.0f;
@@ -153,16 +153,15 @@ void drawGameOverScreen(Font ButtonFont)
 	DrawTextPro(ButtonFont, GameOverButtonMessage3.c_str(),startPosition3, startOrigin, fontRotation, fontSize, fontSpacing, GOLD);
 }
 
-void playGame()
+void playGame(bool gamePaused)
 {
+	Color transparentGrey = {255, 255, 255, 100};
 	int gameSpeed = GetRandomValue(1, 5);
 
-	// Ball properties
 	static Vector2 ballPos = { screenWidth / 2.0f, screenHeight / 2.0f };
 	static Vector2 ballSpeed = { 3.0f, 1.0f };
 	float ballRadius = 10.0f;
 
-    // Paddle properties
 	static float playerOnePaddleY = 250.0f;
 	static float playerTwoPaddleY = 250.0f;
 	float rectangleWidth = 10.0f;
@@ -170,101 +169,110 @@ void playGame()
 	float roundness = 1.0f;
 	int segments = 16;
 
-    // Update ball position
-	ballPos.x += ballSpeed.x;
-	ballPos.y += ballSpeed.y;
-
-	//player one paddle properties
-	if(IsKeyDown(KEY_UP) && playerOnePaddleY >= 10.0f) // the hightest the paddle can go up
+	if(gamePaused)
 	{
-		playerOnePaddleY -= 5.0f; // direction and speed of paddle
+		DrawText(("PAUSED"), 300, 250, 50, RED);
+		DrawText(to_string(playerOneScore).c_str(), 365, 13, 50, transparentGrey);
+		DrawText(to_string(playerTwoScore).c_str(), 410, 13, 50, transparentGrey);
+		DrawCircleV(ballPos, ballRadius, transparentGrey);
+		DrawRectangleRounded({10.0f, playerOnePaddleY, rectangleWidth, rectangleHeight}, roundness, segments, transparentGrey);
+		DrawRectangleRounded({screenWidth - 20, playerTwoPaddleY, rectangleWidth, rectangleHeight}, roundness, segments, transparentGrey);
 	}
-	if(IsKeyDown(KEY_DOWN) && playerOnePaddleY <= 540.0f) //the lowest the paddle can go down (size of padle - edge of screen (650 - 250) + 90)
+	else
 	{
-		playerOnePaddleY += 5.0f; // direction and speed of paddle
-	}
-	
-	//player two paddle properties
-	if (IsKeyDown(KEY_W) && playerTwoPaddleY >= 10.0f) // the hightest the paddle can go up
-	{
-		playerTwoPaddleY -= 5.0f; // direction and speed of paddle
-	}
-	if (IsKeyDown(KEY_S) && playerTwoPaddleY <= 540.0f) //the lowest the paddle can go down (size of padle - edge of screen (650 - 250) + 90)
-	{
-		playerTwoPaddleY += 5.0f; // direction and speed of paddle
-	}
+		ballPos.x += ballSpeed.x;
+		ballPos.y += ballSpeed.y;
 
-	// Ball collision with paddles
-	if(CheckCollisionCircleRec(ballPos, ballRadius, {10.0f, playerOnePaddleY, rectangleWidth, rectangleHeight})) //10 - offset from screen
-	{
-		ballSpeed.x *= -1; // multiply the speed by neg 1 to switch direction
-
-		float hitPos = (ballPos.y - playerOnePaddleY) / rectangleHeight;
-
-		if (hitPos < 0.23f)
+		if(IsKeyDown(KEY_UP) && playerOnePaddleY >= 10.0f)
 		{
-			ballSpeed.y = -fabs(ballSpeed.y) * 1.25f;
-		} else if (hitPos > 0.76f)
-		{ 
-			ballSpeed.y = fabs(ballSpeed.y) * 1.25f;
-		} else 
-		{ 
-			ballSpeed.y *= 1.05f;
+			playerOnePaddleY -= 5.0f;
 		}
-	}
-	if(CheckCollisionCircleRec(ballPos, ballRadius, {780.0f, playerTwoPaddleY, rectangleWidth, rectangleHeight})) //20 - offset from screen
-	{
-		ballSpeed.x *= -1; // multiply the speed by neg 1 to switch direction
-
-		float hitPos = (ballPos.y - playerTwoPaddleY) / rectangleHeight;
-
-		if (hitPos < 0.23f)
+		if(IsKeyDown(KEY_DOWN) && playerOnePaddleY <= 540.0f)
 		{
-			ballSpeed.y = -fabs(ballSpeed.y) * 1.25f;
-		} else if (hitPos > 0.76f)
-		{ 
-			ballSpeed.y = fabs(ballSpeed.y) * 1.25f;
-		} else 
-		{ 
-			ballSpeed.y *= 1.05f;
+			playerOnePaddleY += 5.0f;
 		}
-	}
 
-	// Ball out of bounds
-	if(ballPos.x > screenWidth) // if ball pass screen on right player one point 
-	{
-		playerOneScore++; 
-		ballPos = { screenWidth / 2.0f, screenHeight / 2.0f };
-		playerOnePaddleY = 250.0f;
-		playerTwoPaddleY = 250.0f;
-		ballSpeed.x = GetRandomValue(0, 1) ? 3.0f : -3.0f; // Randomize x-direction
-		ballSpeed.y = GetRandomValue(0, 1) ? 1.0f : -1.0f; // Randomize y-direction
-		WaitTime(1.0f);
-	}
-	if(ballPos.x < 0) // if ball pass screen on left player two point
-	{
-		playerTwoScore++; 
-		ballPos = { screenWidth / 2.0f, screenHeight / 2.0f };
-		playerTwoPaddleY = 250.0f;
-		playerOnePaddleY = 250.0f;
-		ballSpeed.x = GetRandomValue(0, 1) ? 3.0f : -3.0f; // Randomize x-direction
-		ballSpeed.y = GetRandomValue(0, 1) ? 1.0f : -1.0f; // Randomize y-direction
-		WaitTime(1.0f);
-	}
 
-	//ball collision with top and bottom properties
-	if(ballPos.y <= 20)
-	{
-		ballSpeed.y *= -1;
-	}
-	if(ballPos.y >= screenHeight-20)
-	{
-		ballSpeed.y *= -1;
-	}
+		if (IsKeyDown(KEY_W) && playerTwoPaddleY >= 10.0f)
+		{
+			playerTwoPaddleY -= 5.0f;
+		}
+		if (IsKeyDown(KEY_S) && playerTwoPaddleY <= 540.0f)
+		{
+			playerTwoPaddleY += 5.0f;
+		}
 
-	DrawText(to_string(playerOneScore).c_str(), 365, 13, 50, BLUE);
-	DrawText(to_string(playerTwoScore).c_str(), 410, 13, 50, MAGENTA);
-	DrawCircleV(ballPos, ballRadius, WHITE);
-	DrawRectangleRounded({10.0f, playerOnePaddleY, rectangleWidth, rectangleHeight}, roundness, segments, BLUE);
-	DrawRectangleRounded({screenWidth - 20, playerTwoPaddleY, rectangleWidth, rectangleHeight}, roundness, segments, MAGENTA);
+		if(CheckCollisionCircleRec(ballPos, ballRadius, {10.0f, playerOnePaddleY, rectangleWidth, rectangleHeight}))
+		{
+			ballSpeed.x *= -1;
+
+			float hitPos = (ballPos.y - playerOnePaddleY) / rectangleHeight;
+
+			if (hitPos < 0.23f)
+			{
+				ballSpeed.y = -fabs(ballSpeed.y) * 1.25f;
+			} else if (hitPos > 0.76f)
+			{ 
+				ballSpeed.y = fabs(ballSpeed.y) * 1.25f;
+			} else 
+			{ 
+				ballSpeed.y *= 1.05f;
+			}
+		}
+		if(CheckCollisionCircleRec(ballPos, ballRadius, {780.0f, playerTwoPaddleY, rectangleWidth, rectangleHeight}))
+		{
+			ballSpeed.x *= -1;
+
+			float hitPos = (ballPos.y - playerTwoPaddleY) / rectangleHeight;
+
+			if (hitPos < 0.23f)
+			{
+				ballSpeed.y = -fabs(ballSpeed.y) * 1.25f;
+			} else if (hitPos > 0.76f)
+			{ 
+				ballSpeed.y = fabs(ballSpeed.y) * 1.25f;
+			} else 
+			{ 
+				ballSpeed.y *= 1.05f;
+			}
+		}
+
+		if(ballPos.x > screenWidth)
+		{
+			playerOneScore++; 
+			ballPos = { screenWidth / 2.0f, screenHeight / 2.0f };
+			playerOnePaddleY = 250.0f;
+			playerTwoPaddleY = 250.0f;
+			ballSpeed.x = GetRandomValue(0, 1) ? 3.0f : -3.0f;
+			ballSpeed.y = GetRandomValue(0, 1) ? 1.0f : -1.0f;
+			WaitTime(1.0f);
+		}
+		if(ballPos.x < 0)
+		{
+			playerTwoScore++; 
+			ballPos = { screenWidth / 2.0f, screenHeight / 2.0f };
+			playerTwoPaddleY = 250.0f;
+			playerOnePaddleY = 250.0f;
+			ballSpeed.x = GetRandomValue(0, 1) ? 3.0f : -3.0f;
+			ballSpeed.y = GetRandomValue(0, 1) ? 1.0f : -1.0f;
+			WaitTime(1.0f);
+		}
+
+		if(ballPos.y <= 20)
+		{
+			ballSpeed.y *= -1;
+		}
+		if(ballPos.y >= screenHeight-20)
+		{
+			ballSpeed.y *= -1;
+		}
+
+		DrawText(to_string(playerOneScore).c_str(), 365, 13, 50, BLUE);
+		DrawText(to_string(playerTwoScore).c_str(), 410, 13, 50, MAGENTA);
+		DrawText(("Q - QUIT"), 10, 0, 10, transparentGrey);
+		DrawText(("SPACE - PAUSE"), 75, 0, 10, transparentGrey);
+		DrawCircleV(ballPos, ballRadius, WHITE);
+		DrawRectangleRounded({10.0f, playerOnePaddleY, rectangleWidth, rectangleHeight}, roundness, segments, BLUE);
+		DrawRectangleRounded({screenWidth - 20, playerTwoPaddleY, rectangleWidth, rectangleHeight}, roundness, segments, MAGENTA);
+	}
 }
